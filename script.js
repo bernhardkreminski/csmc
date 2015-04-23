45406f09b0ecbe281840ab90923b48ee67936eb5
Gohrlug <lukaskellerer1@googlemail.com>
2015-04-23 11:13:45 +0200

    jquery hinzugefügt

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-window.onload = function() {
-    
-    
-}
+$("#kontakt").hide();
+$("#portfolio").hide();
+
+$("#nav li:first-child").click(function () {
+    $("#kontakt").hide();
+    $("#portfolio").hide();
+    $("#ueberuns").hide();
+    $("#content").show();
+});
+
+$("#link1").click(function () {
+    $("#kontakt").hide();
+    $("#portfolio").hide();
+    $("#ueberuns").show();
+    $("#content").hide();
+});
+
+$("#link2").click(function () {
+    $("#kontakt").hide();
+    $("#portfolio").show();
+    $("#ueberuns").hide();
+    $("#content").hide();
+});
+
+$("#link3").click(function () {
+    $("#kontakt").show();
+    $("#portfolio").hide();
+    $("#ueberuns").hide();
+    $("#content").hide();
+});
