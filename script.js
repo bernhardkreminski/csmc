4f105814da29a1a2284629de139b965073ccd201
KilianKae <Kilian@Kilians-MacBook-Pro.local>
2015-04-26 18:27:08 +0200

    Clean Up/Neue Navigation

--- script.js
+++ script.js
@@ -1,32 +1,62 @@
-$("#kontakt").hide();
-$("#portfolio").hide();
-$("#ueberuns").hide();
-$("#home").show();
-
-$("#nav li:first-child").click(function () {
+$(document).ready(function(){
+    var isShown = $("#home");
     $("#kontakt").hide();
     $("#portfolio").hide();
     $("#ueberuns").hide();
     $("#home").show();
-});
 
-$("#link1").click(function () {
-    $("#kontakt").hide();
-    $("#portfolio").hide();
-    $("#ueberuns").show();
-    $("#home").hide();
-});
+    $("#nav li:first-child").click(function () {
+        moveDown(isShown,$("#home"));
+    });
 
-$("#link2").click(function () {
-    $("#kontakt").hide();
-    $("#portfolio").show();
-    $("#ueberuns").hide();
-    $("#home").hide();
-});
+    $("#link1").click(function () {
+        moveDown(isShown,$("#ueberuns"));
+    });
 
-$("#link3").click(function () {
-    $("#kontakt").show();
-    $("#portfolio").hide();
-    $("#ueberuns").hide();
-    $("#home").hide();
+    $("#link2").click(function () {
+        moveDown(isShown,$("#portfolio"));
+    });
+
+    $("#link3").click(function () {
+        moveDown(isShown,$("#kontakt"));
+    });
+    
+    function moveDown (object1, object2) {
+        if(object1.is(object2)){
+            var img = object1.find("img");
+            img.animate(
+            {   
+                opacity: "0.4",
+                filter: "alpha(opacity=40)"
+            },
+            300, 
+            function() { 
+            img.animate(
+                {   
+                opacity: "1",
+                filter: "alpha(opacity=100)"
+                },
+                300)});
+        }
+        else{
+        isShown = object2;
+        object2.css("top","-100%");
+        object2.show();
+        object1.animate(
+            {
+                top: "+=100%"
+            },
+            2000,
+            function(){
+                object1.hide();}
+        );
+        object2.animate(
+            {
+                top: "+=100%"
+            },
+            2000
+            );
+        }
+    };
+
 });
