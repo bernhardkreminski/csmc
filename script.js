1bb1c57a1fca09ffcd1d2d6e94ec15238edbbe59
KilianKae <Kilian@Kilians-MacBook-Pro.local>
2015-04-26 16:10:25 +0200

    Überarbeitung/Zusammenfassung

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 $("#kontakt").hide();
 $("#portfolio").hide();
 $("#ueberuns").hide();
+$("#home").show();
 
 $("#nav li:first-child").click(function () {
     $("#kontakt").hide();
     $("#portfolio").hide();
     $("#ueberuns").hide();
-    $("#content").show();
+    $("#home").show();
 });
 
 $("#link1").click(function () {
     $("#kontakt").hide();
     $("#portfolio").hide();
     $("#ueberuns").show();
-    $("#content").hide();
+    $("#home").hide();
 });
 
 $("#link2").click(function () {
     $("#kontakt").hide();
     $("#portfolio").show();
     $("#ueberuns").hide();
-    $("#content").hide();
+    $("#home").hide();
 });
 
 $("#link3").click(function () {
     $("#kontakt").show();
     $("#portfolio").hide();
     $("#ueberuns").hide();
-    $("#content").hide();
+    $("#home").hide();
 });
