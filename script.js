f4072328b7c61051c0cc03f6144bf40095b63c64
KilianKae <Kilian@Kilians-MacBook-Pro.local>
2015-04-29 19:59:29 +0200

    Aufblinken bei gleicher seit

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+    var onDisplay = $("#home");
+
     function scrollToElement (element) {
-        // get Position of Element
-        var xPosition = element.position().top;   
-        scrollElement ($('#content'), xPosition, 2);
+        if (!element.is(onDisplay)){
+            // get Position of Element
+            var xPosition = element.position().top;   
+            scrollElement ($('#content'), xPosition, 2);
+        }
+        else {
+        whiteFlash(element, 0.4);    
+        }
+        onDisplay = element;
     }
     function scrollElement (element, scrollPosition, duration) {
 
@@ -14,5 +22,19 @@
         // use translate3d to force hardware acceleration
         element.css("webkitTransform" ,'translate3d(0, ' + -scrollPosition + 'px, 0)');
         element.css("Transform" ,'translate3d(0, ' + -scrollPosition + 'px, 0)');
+    }
+    function whiteFlash (element, duration) {
+        var normalOp = element.css("opacity");
+        
+        element.css("webkitTransition", duration + 's');
+        element.css("transition", duration + 's');
+        element.css("webkitTransitionTimingFunction" ,'ease-in-out');
+        element.css("TransitionTimingFunction", 'ease-in-out');
+
+        // use translate3d to force hardware acceleration
+        element.css("opacity" , 0.8);
+        setTimeout(function(){
+            element.css("opacity", normalOp);
+        }, duration * 1000);
     }
 
