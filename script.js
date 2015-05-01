96c743062813e0a7641dd32342b4c41873ee1cac
KilianKae <Kilian@Kilians-MacBook-Pro.local>
2015-05-01 10:19:55 +0200

    Scrolling gefixed/Neuer Versuch bei portfolio

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
-    var onDisplay = $("#home");
 
-    function scrollToElement (element) {
-        if (!element.is(onDisplay)){
-            // get Position of Element
-            var xPosition = element.position().top;   
-            scrollElement ($('#content'), xPosition, 2);
-        }
-        else {
+$(document).ready(function() {
+    $(window).scroll(function(){
+        $(this).scrollTop(top).scrollLeft(left);
+    });
+});
+
+var onDisplay = $("#home");
+
+function scrollToElement (element) {
+    if (!element.is(onDisplay)){
+        // get Position of Element
+        var xPosition = element.position().top;   
+        scrollElement ($('#content'), xPosition, 2);
+    }
+    else {
         whiteFlash(element, 0.4);    
-        }
-        onDisplay = element;
     }
-    function scrollElement (element, scrollPosition, duration) {
+    onDisplay = element;
+}
+function scrollElement (element, scrollPosition, duration) {
 
-        // setup CSS transition duration and easing function
-        element.css("webkitTransition", duration + 's');
-        element.css("transition", duration + 's');
-        element.css("webkitTransitionTimingFunction" ,'ease-in-out');
-        element.css("TransitionTimingFunction", 'ease-in-out');
+    // setup CSS transition duration and easing function
+    element.css("webkitTransition", duration + 's');
+    element.css("transition", duration + 's');
+    element.css("webkitTransitionTimingFunction" ,'ease-in-out');
+    element.css("TransitionTimingFunction", 'ease-in-out');
 
-        // use translate3d to force hardware acceleration
-        element.css("webkitTransform" ,'translate3d(0, ' + -scrollPosition + 'px, 0)');
-        element.css("Transform" ,'translate3d(0, ' + -scrollPosition + 'px, 0)');
-    }
-    function whiteFlash (element, duration) {
-        var normalOp = element.css("opacity");
-        
-        element.css("webkitTransition", duration + 's');
-        element.css("transition", duration + 's');
-        element.css("webkitTransitionTimingFunction" ,'ease-in-out');
-        element.css("TransitionTimingFunction", 'ease-in-out');
+    // use translate3d to force hardware acceleration
+    element.css("webkitTransform" ,'translate3d(0, ' + -scrollPosition + 'px, 0)');
+    element.css("Transform" ,'translate3d(0, ' + -scrollPosition + 'px, 0)');
+}
+function whiteFlash (element, duration) {
+    var normalOp = element.css("opacity");
 
-        // use translate3d to force hardware acceleration
-        element.css("opacity" , 0.8);
-        setTimeout(function(){
-            element.css("opacity", normalOp);
-        }, duration * 1000);
-    }
+    element.css("webkitTransition", duration + 's');
+    element.css("transition", duration + 's');
+    element.css("webkitTransitionTimingFunction" ,'ease-in-out');
+    element.css("TransitionTimingFunction", 'ease-in-out');
+
+    // use translate3d to force hardware acceleration
+    element.css("opacity" , 0.8);
+    setTimeout(function(){
+        element.css("opacity", normalOp);
+    }, duration * 1000);
+}
 
